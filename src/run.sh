f54d7f15157a5d76ca0765a0f476333f167b9296
lh <theophilus03@foxmail.com>
2025-08-14 16:44:22 +0800

    Modify run.sh

--- src/run.sh
+++ src/run.sh
@@ -10,11 +10,8 @@ export PYTHONPATH= #Your PYTHONPATH
 export OPENAI_API_KEY= #Your OPENAI API Key
 export OPENAI_BASE_URL= #OPENAI Base URL
 
-# export OPENAI_BASE_URL="https://www.gptapi.us/v1"
-# export OPENAI_API_KEY=sk-YuzZ2NMCE63opFYn22Ae6d3b015c42D49a0d4008B6780fD4
-
 # Run the first ROS node in the background
-# roslaunch chatstitch chat.launch &
+roslaunch chatstitch chat.launch &
 sleep 5
  roslaunch ngp_render ngp_render.launch &
  sleep 5
